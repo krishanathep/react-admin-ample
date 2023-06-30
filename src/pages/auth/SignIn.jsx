dd02665a2232cebf616fe2ecca18b0fe21448813
krishanathep@gmail.com <ap4imanee@gmail.com>
2023-06-30 10:22:02 +0700

    fix vercel error

--- src/pages/auth/SignIn.jsx
+++ src/pages/auth/SignIn.jsx
@@ -38,90 +38,47 @@ function SignIn() {
   }
 
   return (
-  <div className="main-wrapper">
-  <div className="auth-wrapper d-flex no-block justify-content-center align-items-center" style={{background: 'url(src/assets/ample/src/assets/images/big/auth-bg.jpg) no-repeat center center'}}>
-    <div className="auth-box p-4 bg-white rounded">
-      <div id="loginform">
-      <div className="logo text-center">
-          <span className="db"><img src="ample/src/assets/images/logos/logo-icon.png" alt="logo" /></span>
-          <h5 className="font-weight-medium mb-3 mt-1">Sign In to System</h5>
-        </div>
-        <div className="row">
-          <div className="col-12">
-            <form className="form-horizontal mt-3" id="loginform" onSubmit={handleSubmit(onSubmit)}>
-            <div className="form-floating mb-3">
-                <input type="email" className="form-control form-input-bg" id="tb-rfname" placeholder="john deo" {...register("email", { required: true })} required  />
-                <label htmlFor="tb-rfname">Email</label>
-                <div className="invalid-feedback">
-                  Full email is required
-                </div>
-              </div>
-              <div className="form-floating mb-3">
-                <input type="password" className="form-control form-input-bg" id="tb-rfname" placeholder="john deo" {...register("password", { required: true })} required />
-                <label htmlFor="tb-rfname">Password</label>
-                <div className="invalid-feedback">
-                  Full password is required
-                </div>
-              </div>
-              <div className="form-group">
-                <div className="d-flex">
-                  <div className="checkbox checkbox-info pt-0">
-                    <input id="checkbox-signup" type="checkbox" className="material-inputs chk-col-indigo" />
-                    <label htmlFor="checkbox-signup"> Remember me </label>
+    <>
+    <div className="auth-wrapper d-flex no-block justify-content-center align-items-center" style={{background: 'url(/ample/src/assets/images/big/auth-bg.jpg) no-repeat center center'}}>
+      <div className="auth-box p-4 bg-white rounded">
+        <div>
+          <div className="logo text-center">
+            <span className="db"><img src="/ample/src/assets/images/logos/logo-icon.png" alt="logo" /></span>
+            <h5 className="font-weight-medium mb-3 mt-1">Sign In to Admin</h5>
+          </div>
+          <div className="row mt-4">
+            <div className="col-12">
+              <form className="form-horizontal" onSubmit={handleSubmit(onSubmit)}>
+                <div className="form-floating mb-3">
+                  <input type="email" className="form-control form-input-bg" id="tb-remail" placeholder="john@gmail.com" {...register("email", { required: true })} />
+                  <label htmlFor="tb-remail">Email</label>
+                  <div className="invalid-feedback">
+                    Email is required
                   </div>
-                  <div className="ms-auto">
-                    <a href="javascript:void(0)" id="to-recover" className="link font-weight-medium"><i className="fa fa-lock me-1" /> Forgot pwd?</a>
+                  {errors.email && <span className="text-danger">This Email field is required</span>}
+                </div>
+                <div className="form-floating mb-3">
+                  <input type="password" className="form-control form-input-bg" id="text-rpassword" placeholder="*****" {...register("password", { required: true })} />
+                  <label htmlFor="text-rpassword">Password</label>
+                  <div className="invalid-feedback">
+                    Password is required
                   </div>
+                  {errors.password && <span className="text-danger">This Passwrd field is required</span>}
                 </div>
-              </div>
-              <div className="form-group text-center mt-4 mb-3">
-                <div className="col-xs-12">
-                  <button className="btn btn-info d-block w-100 waves-effect waves-light" type="submit">Log In</button>
+                <div className="checkbox checkbox-primary mb-3">
+                  <input id="checkbox-signup" type="checkbox" className="chk-col-indigo material-inputs" />
+                  <label htmlFor="checkbox-signup"> I agree to all <a href="#">Terms</a></label>
                 </div>
-              </div>
-              {/* <div className="row">
-                <div className="col-xs-12 col-sm-12 col-md-12 mt-2 text-center">
-                  <div className="social mb-3">
-                    <a href="javascript:void(0)" className="btn  btn-facebook" data-bs-toggle="tooltip" title="Login with Facebook"> <i aria-hidden="true" className="fab fa-facebook-f" /> </a>{' '}
-                    <a href="javascript:void(0)" className="btn btn-googleplus" data-bs-toggle="tooltip" title="Login with Google"> <i aria-hidden="true" className="fab fa-google" /> </a>
-                  </div>
+                <div className="d-flex align-items-stretch">
+                  <button type="submit" className="btn btn-info d-block w-100">Sign up</button>
                 </div>
-              </div> */}
-              <div className="form-group mb-0 mt-4">
-                <div className="col-sm-12 justify-content-center d-flex">
-                  <p>Register a new membership <Link to={'/signup'}  className="text-info font-weight-medium ms-1">Sign Up</Link></p>
-                </div>
-              </div>
-            </form>
+              </form>
+            </div>
           </div>
         </div>
       </div>
-      <div id="recoverform">
-        <div className="logo">
-          <h3 className="font-weight-medium mb-3">Recover Password</h3>
-          <span className="text-muted">Enter your Email and instructions will be sent to you!</span>
-        </div>
-        <div className="row mt-3 form-material">
-          {/* Form */}
-          <form className="col-12" action="index.html">
-            {/* email */}
-            <div className="form-group row">
-              <div className="col-12">
-                <input className="form-control" type="email" required placeholder="Username" />
-              </div>
-            </div>
-            {/* pwd */}
-            <div className="row mt-3">
-              <div className="col-12">
-                <button className="btn d-block w-100 btn-primary text-uppercase" type="submit" name="action">Reset</button>
-              </div>
-            </div>
-          </form>
-        </div>
-      </div>
     </div>
-  </div>
-</div>
+  </>
   )
 }
 
--- src/pages/auth/SignUp.jsx
+++ src/pages/auth/SignUp.jsx
@@ -3,64 +3,60 @@ import { Link } from 'react-router-dom'
 
 function SignUp() {
   return (
-  <div className="main-wrapper">
-  <div className="auth-wrapper d-flex no-block justify-content-center align-items-center" style={{background: 'url(src/assets/ample/src/assets/images/big/auth-bg.jpg) no-repeat center center'}}>
-    <div className="auth-box p-4 bg-white rounded">
-      <div>
-        <div className="logo text-center">
-          <span className="db"><img src="ample/src/assets/images/logos/logo-icon.png" alt="logo" /></span>
-          <h5 className="font-weight-medium mb-3 mt-1">Sign Up to Admin</h5>
-        </div>
-        <div className="row mt-4">
-          <div className="col-12">
-            <form className="form-horizontal" action="index.html">
-              <div className="form-floating mb-3">
-                <input type="text" className="form-control form-input-bg" id="tb-rfname" placeholder="john deo" required />
-                <label htmlFor="tb-rfname">Full Name</label>
-                <div className="invalid-feedback">
-                  Full name is required
+  <>
+    <div className="auth-wrapper d-flex no-block justify-content-center align-items-center" style={{background: 'url(/ample/src/assets/images/big/auth-bg.jpg) no-repeat center center'}}>
+      <div className="auth-box p-4 bg-white rounded">
+        <div>
+          <div className="logo text-center">
+            <span className="db"><img src="/ample/src/assets/images/logos/logo-icon.png" alt="logo" /></span>
+            <h5 className="font-weight-medium mb-3 mt-1">Sign Up to Admin</h5>
+          </div>
+          {/* Form */}
+          <div className="row mt-4">
+            <div className="col-12">
+              <form className="form-horizontal" action="index.html">
+                <div className="form-floating mb-3">
+                  <input type="text" className="form-control form-input-bg" id="tb-rfname" placeholder="john deo" required />
+                  <label htmlFor="tb-rfname">Full Name</label>
+                  <div className="invalid-feedback">
+                    Full name is required
+                  </div>
                 </div>
-              </div>
-              <div className="form-floating mb-3">
-                <input type="email" className="form-control form-input-bg" id="tb-remail" placeholder="john@gmail.com" required />
-                <label htmlFor="tb-remail">Email</label>
-                <div className="invalid-feedback">
-                  Email is required
+                <div className="form-floating mb-3">
+                  <input type="email" className="form-control form-input-bg" id="tb-remail" placeholder="john@gmail.com" required />
+                  <label htmlFor="tb-remail">Email</label>
+                  <div className="invalid-feedback">
+                    Email is required
+                  </div>
                 </div>
-              </div>
-              <div className="form-floating mb-3">
-                <input type="password" className="form-control form-input-bg" id="text-rpassword" placeholder="*****" required />
-                <label htmlFor="text-rpassword">Password</label>
-                <div className="invalid-feedback">
-                  Password is required
+                <div className="form-floating mb-3">
+                  <input type="password" className="form-control form-input-bg" id="text-rpassword" placeholder="*****" required />
+                  <label htmlFor="text-rpassword">Password</label>
+                  <div className="invalid-feedback">
+                    Password is required
+                  </div>
                 </div>
-              </div>
-              <div className="form-floating mb-3">
-                <input type="password" className="form-control form-input-bg" id="text-rcpassword" placeholder="*****" required />
-                <label htmlFor="text-rcpassword">Confirm Password</label>
-                <div className="invalid-feedback">
-                  Password is required
+                <div className="form-floating mb-3">
+                  <input type="password" className="form-control form-input-bg" id="text-rcpassword" placeholder="*****" required />
+                  <label htmlFor="text-rcpassword">Confirm Password</label>
+                  <div className="invalid-feedback">
+                    Password is required
+                  </div>
                 </div>
-              </div>
-              <div className="checkbox checkbox-primary mb-3">
-                <input id="checkbox-signup" type="checkbox" className="chk-col-indigo material-inputs" />
-                <label htmlFor="checkbox-signup"> I agree to all <a href="#">Terms</a></label>
-              </div>
-              <div className="d-flex align-items-stretch">
-                <button type="submit" className="btn btn-info d-block w-100">Sign up</button>
-              </div>
-              <div className="form-group mb-0 mt-4">
-                <div className="col-sm-12 justify-content-center d-flex">
-                  <p>I already have a membership <Link to={'/signin'}  className="text-info font-weight-medium ms-1">Sign Up</Link></p>
+                <div className="checkbox checkbox-primary mb-3">
+                  <input id="checkbox-signup" type="checkbox" className="chk-col-indigo material-inputs" />
+                  <label htmlFor="checkbox-signup"> I agree to all <a href="#">Terms</a></label>
                 </div>
-              </div>
-            </form>
+                <div className="d-flex align-items-stretch">
+                  <button type="submit" className="btn btn-info d-block w-100">Sign up</button>
+                </div>
+              </form>
+            </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
-</div>
+  </>
 )
 }
 
